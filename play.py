7fd651af38c61bb0cade7ab4240b1e41a9af9fab
maxv2009 <151852484+maxv2009@users.noreply.github.com>
2024-07-08 15:00:17 +0200

    Added PyTorch for performance increase

--- play.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-from gymnasium.utils import play
-
-def main():
-    env = gym.make('LunarLander-v2', render_mode="human")
-    gym.utils.play.play(env, zoom=3)
-
-if __name__ == "__main__":
-    main()
